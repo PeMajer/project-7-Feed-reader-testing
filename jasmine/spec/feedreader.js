150b92a70d65d91f4ad08ad0432d8c1b6c2ae7d2
PeMajer <majer.petr@gmail.com>
2018-03-18 11:23:16 +0100

    pridani testu - zda pocatecni feed obsahuje alespon jeden odkaz

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -72,7 +72,7 @@ $(function() {
           */
 
         it('change visibility on click', function() {
-            var icon = $('.menu-icon-link');
+            const icon = $('.menu-icon-link');
 
             icon.click(); //display menu
             expect($('body').hasClass('menu-hidden')).not.toBe(true);
@@ -84,13 +84,24 @@ $(function() {
 
     });
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('contain at least one feed.', function(done){
+            expect($('.entry').length).not.toBe(0);
+            done();
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
